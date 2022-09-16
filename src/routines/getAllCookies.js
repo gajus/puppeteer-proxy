600a6218b839a3bcf2d2b155ab920e84ce2603c3
natpacket <1399092738@qq.com>
2022-09-16 10:41:22 +0800

    fix: fix syntax err

--- src/routines/getAllCookies.js
+++ src/routines/getAllCookies.js
@@ -6,6 +6,6 @@ import type {Page} from 'puppeteer';
  * @see https://stackoverflow.com/a/59604510/368691
  */
 export default (page: Page) => {
-  let client = typeof page._client == "function" ? page._client() : page._client;
+  const client = typeof page._client === 'function' ? page._client() : page._client;
   return client.send('Network.getAllCookies');
 };
